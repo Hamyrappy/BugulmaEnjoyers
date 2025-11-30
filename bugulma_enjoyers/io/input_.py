11da77cddc973613b1264803137f2b1e285fc619
OstarkovSN <ostarkovsn@gmail.com>
2025-11-30 20:28:09 +0300

    [fix] no ID error

--- bugulma_enjoyers/io/input_.py
+++ bugulma_enjoyers/io/input_.py
@@ -6,9 +6,13 @@ REQUIRED_COL = "tat_toxic"
 
 COL_NOT_FOUND = "Column '{column}' not found in the input file."
 
+
 def read_input(path: str | Path, column: str = REQUIRED_COL) -> str:
     path = Path(path)
-    df = pd.read_csv(str(path.resolve().absolute()), sep="\t", index_col="ID")
+    try:
+        df = pd.read_csv(str(path.resolve().absolute()), sep="\t", index_col="ID")
+    except ValueError:
+        df = pd.read_csv(str(path.resolve().absolute()), sep="\t")
     if column not in df.columns:
         raise ValueError(COL_NOT_FOUND.format(column=column))
     return df[column].to_list()
