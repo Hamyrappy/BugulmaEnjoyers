210e45683d205bdc7161ec2b1a172e77dbc38495
OstarkovSN <ostarkovsn@gmail.com>
2025-11-30 21:48:56 +0300

    [fix] double-ID error

--- bugulma_enjoyers/io/output.py
+++ bugulma_enjoyers/io/output.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 def write_output(results: list[str], inputs: list[str], path: str | Path) -> None:
     path = Path(path)
-    df = pd.DataFrame({"ID": range(len(inputs)), "tat_toxic": inputs, "tat_detox1": results})
+    df = pd.DataFrame({"tat_toxic": inputs, "tat_detox1": results})
 
     df.to_csv(str(path.absolute().resolve()), sep="\t", index_label="ID", encoding="utf-8")
-    
