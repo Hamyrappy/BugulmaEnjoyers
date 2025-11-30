d81f9809dae338ad82dac1aa146c681c2dcb7c67
OstarkovSN <ostarkovsn@gmail.com>
2025-11-30 19:28:01 +0300

    [added] final mail.py & dev_inputs

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 """Main entrypoint for BugulmaEnjoyers."""
 
-from pathlib import Path
-
 import click
 
 # from bugulma_enjoyers.detoxifiers import TheOneAndSuperDetoxifierWeFinallySelected # noqa: ERA001
+from bugulma_enjoyers.detoxifiers import PipelineConfig, StandaloneDetoxifier
+from bugulma_enjoyers.io import read_input, write_output
+from bugulma_enjoyers.setup_logging import setup_logging
 
 
-@click.option("--file", "-f", help="File to read.", default="data.txt")
-@click.option("--output", "-o", help="File to write.", default="result.txt")
+@click.option("--verbose", "-v", count=True, default=False)
+@click.option("--quiet", "-q", count=True, default=False)
+@click.option("--file", "-f", help="File to read.", default="dev_inputs.tsv")
+@click.option("--output", "-o", help="File to write.", default="dev_outputs.tsv")
 @click.command()
-def main(file: str = "data.txt", output: str = "result.txt") -> None:
+def main(
+    file: str = "dev_inputs.tsv",
+    output: str = "dev_outputs.tsv",
+    verbose: int = 0,
+    quiet: int = 0,
+) -> None:
     """Main entrypoint for BugulmaEnjoyers."""
-    fp = Path(file)
-    with fp.open() as f:
-        texts = [line.strip() for line in f]
-    results = texts  # TheOneAndSuperDetoxifierWeFinallySelected.detoxify_batch(texts)
-    with Path(output).open("w") as f:
-        f.write("\n".join(results))
+    verbosity = verbose - quiet +1
+    setup_logging(verbosity)
+    texts = read_input(file)
+    config = PipelineConfig()
+    detox = StandaloneDetoxifier(config)
+    results = detox.detoxify_batch(texts)
+    write_output(results, texts, output)
 
 
 if __name__ == "__main__":
